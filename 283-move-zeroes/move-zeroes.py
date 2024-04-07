783252ba42c27e55df2d6850824df8714c5fcc4c
Jyothi V <57355660+jyothiv-codes@users.noreply.github.com>
2024-04-07 14:56:10 -0700

    Time: 126 ms (32.47%) | Memory: 13 MB (12.49%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        count=0
+        i=0
+        while i<len(nums):
+            if nums[i]==0:
+                nums.pop(i)
+                count+=1
+            else:
+                i+=1
+        i=0
+        while i<count:
+            nums.append(0)
+            i+=1
+    
+        
