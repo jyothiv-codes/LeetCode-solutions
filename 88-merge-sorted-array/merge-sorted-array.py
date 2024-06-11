8fec3cc119a2147df9eb435521dab19fe0e50d02
Jyothi V <57355660+jyothiv-codes@users.noreply.github.com>
2024-06-10 19:07:52 -0700

    Time: 25 ms (15.61%) | Memory: 11.5 MB (74.03%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -7,11 +7,8 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        i=0
-        j=0
-        k=0
+        i,j,k=0,0,0
         while j<len(nums2) and i<m+n:
-            #print(nums1)
             if nums2[j]<nums1[i]:
                 nums1.insert(i,nums2[j])
                 nums1.pop()
@@ -20,14 +17,10 @@ class Solution(object):
                 k+=1
             else:
                 i+=1
-        print(nums1,i,j)
         i=m+k
-        print(nums1,i,j)
         while j<n:
-            print(nums1,i,j,"within final loop")
             nums1[i]=nums2[j]
             i+=1
             j+=1
-        print(nums1,i,j)
 
         
