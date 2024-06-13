a3336305998ca2d5469a401e96258d572d053a21
Jyothi V <57355660+jyothiv-codes@users.noreply.github.com>
2024-06-12 18:08:24 -0700

    Time: 456 ms (6.45%) | Memory: 11.6 MB (100.00%) - LeetSync

---
+++ 686-repeated-string-match/repeated-string-match.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def repeatedStringMatch(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: int
+        """
+        a_len=len(a)
+        b_len=len(b)
+        new_s=a
+        i=1
+        repeat=int(len(b)/len(a))
+        while i<=(repeat+5):
+            if b in a*i:
+                return i
+            else:
+                i+=1
+        return -1
+            
+        
+
+        return -1
+        
