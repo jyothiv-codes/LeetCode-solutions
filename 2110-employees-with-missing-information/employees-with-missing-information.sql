ea8355d2b7e10259f8dd43169b2f2afcace7f985
Jyothi V <57355660+jyothiv-codes@users.noreply.github.com>
2024-07-21 00:06:24 -0700

    Time: 483 ms (82.80%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -1,12 +1,9 @@
 # Write your MySQL query statement below
-
 SELECT e.employee_id
 FROM Employees AS e
 LEFT JOIN Salaries AS s ON s.employee_id = e.employee_id 
 WHERE s.salary IS NULL
-
 UNION
-
 SELECT s.employee_id
 FROM Employees AS e
 RIGHT JOIN Salaries AS s ON s.employee_id = e.employee_id 
