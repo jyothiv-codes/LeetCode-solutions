81a6037ab6a4c5750a63115bb4bc55ff2b1183c6
Jyothi V <57355660+jyothiv-codes@users.noreply.github.com>
2024-09-06 18:48:44 -0700

    Time: 44 ms (13.01%) | Memory: 16.9 MB (17.76%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,16 +1,21 @@
-class Solution(object):
-    def reverseWords(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        split_words=s.split(" ")
-        split_words=split_words[::-1]
-        print(split_words)
-        ans=""
-        for word in split_words:
-            if word!="":
-                ans+=word
-                ans+=" "
-        return ans[:-1]
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words=[]
+        word=""
+        for ch in s:
+            if ch==' ':
+                print(word)
+                words.append(word)
+                word=""
+            else:
+                word+=ch
+        words.append(word)
+        words=words[::-1]
+        print(words)
+        output_s=""
+        for word in words:
+            if word!='':
+                output_s+=word
+                output_s+=" "
+        return output_s[:-1]
         
