5bfff81aac2a4ca0b98f8244c9d3414710d50cbf
Jyothi V <57355660+jyothiv-codes@users.noreply.github.com>
2025-02-11 07:46:49 -0800

    Time: 28 ms (95.70%) | Memory: 19.8 MB (77.01%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -1,20 +1,16 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         @lru_cache(None)
-        def edit(word1,word2,m,n):
+        def calculate(word1,word2,m,n):
             if m==0:
                 return n
             if n==0:
                 return m
             if word1[m-1]==word2[n-1]:
-                return edit(word1,word2,m-1,n-1)
+                return calculate(word1,word2,m-1,n-1)
             else:
-                return 1+min(
-                    edit(word1,word2,m,n-1),
-                    edit(word1,word2,m-1,n),
-                    edit(word1,word2,m-1,n-1)
-                )
-        
-
-        return edit(word1,word2,len(word1),len(word2))
+                return 1+min(calculate(word1,word2,m-1,n-1),
+                calculate(word1,word2,m,n-1),
+                calculate(word1,word2,m-1,n))
+        return calculate(word1,word2,len(word1),len(word2))
         
