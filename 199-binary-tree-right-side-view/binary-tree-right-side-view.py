2b623a130ee910bd674523dc58020919b81b4915
Jyothi V <57355660+jyothiv-codes@users.noreply.github.com>
2025-03-04 07:34:51 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (38.56%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -1,32 +1,26 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def rightSideView(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[int]
-        """
-        queue=[]
-        final_ans=[]
-        temp=[]
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         if root is None:
-            return 
-        queue.append(root)
-        while len(queue)>0:
+            return []
+        queue=deque([root])
+        ans=[]
+        while queue:
             n=len(queue)
-            temp=[]
             for i in range(n):
-                curr=queue.pop(0)
-                temp.append(curr.val)
-                if curr.left is not None:
-                    queue.append(curr.left)
-                if curr.right is not None:
-                    queue.append(curr.right)
-            if len(temp)>0:
-                final_ans.append(temp[-1])
-        return final_ans
+                node=queue.popleft()
+                if i==0:
+                    ans.append(node.val)
+                if node.right:
+                    queue.append(node.right)
+                if node.left:
+                    queue.append(node.left)
+        return ans
             
+        
+        
