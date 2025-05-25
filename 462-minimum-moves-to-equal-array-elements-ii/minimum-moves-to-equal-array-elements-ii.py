91206b5027925142e65dc50add5eabe76ff8bd1a
Jyothi V <57355660+jyothiv-codes@users.noreply.github.com>
2025-05-25 16:26:11 -0700

    Time: 7 ms (17.25%) | Memory: 18.8 MB (74.01%) - LeetSync

--- 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.py
+++ 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.py
@@ -1,20 +1,17 @@
 class Solution:
     def minMoves2(self, nums: List[int]) -> int:
         n=len(nums)
-        nums.sort()
-        print(nums)
         if n==1:
             return 0
         if n==2:
-            middle=int((nums[0]+nums[1])/2)
-            print("middle",middle)
-        else:    
-            mid=int(n/2)
-            if n%2!=0:
-                middle=nums[mid]
-            else:
-                middle=int((nums[mid]+nums[mid-1])/2)
-            print("middle to reach",middle)
+            return int((nums[0]+nums[1])/2)
+        nums.sort()
+        print(nums)
+        if n%2==0:
+            middle=int((nums[int((n-1)/2)]+nums[int(n/2)])/2)
+        else:
+            middle=nums[int(n/2)]
+        print("to reach",middle)
         total=0
         for i in nums:
             if i>middle:
@@ -22,4 +19,3 @@ class Solution:
             elif i<middle:
                 total+=(middle-i)
         return total
-        
