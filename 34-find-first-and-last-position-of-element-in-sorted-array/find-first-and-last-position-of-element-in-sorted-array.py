86d1ecf9035a935844583eb5adbf7f4a77443590
Jyothi V <57355660+jyothiv-codes@users.noreply.github.com>
2025-05-31 22:05:13 -0700

    Time: 0 ms (100.00%) | Memory: 19.2 MB (20.91%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,38 +1,41 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def lowerLimit(target):
-            low=0
+        def lowerLimit(low,high):
             index=-1
-            high=len(nums)-1
             while low<=high:
-                mid=int((low+high)/2)
-                if nums[mid]==target:
+                mid=int(int(low+high)/2)
+                if nums[mid]<target:
+                    low=mid+1
+                elif nums[mid]>target:
+                    high=mid-1
+                else:
                     index=mid
                     high=mid-1
-                elif target<nums[mid]:
-                    high=mid-1
-                elif target>nums[mid]:
-                    low=mid+1
             return index
-        def upperLimit(target):
-            low=0
+        def higherLimit(low,high):
             index=-1
-            high=len(nums)-1
             while low<=high:
-                mid=int((low+high)/2)
-                if target==nums[mid]:
+                mid=int(int(low+high)/2)
+                if nums[mid]<target:
+                    low=mid+1
+                elif nums[mid]>target:
+                    high=mid-1
+                else:
                     index=mid
                     low=mid+1
-                elif target>nums[mid]:
-                    low=mid+1
-                elif target<nums[mid]:
-                    high=mid-1
             return index
-        low=lowerLimit(target)
-        high=upperLimit(target)
-        return [low,high]
-        
 
-        
+                
+                
+        if target not in nums or len(nums)==0:
+            return [-1,-1]
+        if len(nums)==1:
+            return [0,0]
+        low=0
+        high=len(nums)-1
+        lower=lowerLimit(low,high)
+        higher=higherLimit(low,high)
+        return [lower,higher]
+
 
         
