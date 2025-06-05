bfa1704a426260a387bbe417de4b052020449bed
Jyothi V <57355660+jyothiv-codes@users.noreply.github.com>
2025-06-05 12:54:39 -0700

    Time: 77 ms (5.10%) | Memory: 18.5 MB (98.82%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,41 +1,32 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        total_collisions=0
-        flag=0
-        while flag==0:
+        collide=0
+        
+        while True:
             i=0
-            this_collisions=0
-            while i<len(asteroids)-1:
-                if i>=0 and asteroids[i]>0 and asteroids[i+1]>0:
-                    pass
-                elif i>=0 and asteroids[i]<0 and asteroids[i+1]<0:
-                    pass
-                else:
-                    
-                    if i>=0 and asteroids[i]>0 and asteroids[i+1]<0:
-                        #print("here",i)
-                        if i>=0 and abs(asteroids[i])==abs(asteroids[i+1]) and asteroids[i]!=asteroids[i+1]:
-                            asteroids.pop(i)
-                            asteroids.pop(i)
-                            i-=2
-                        else:
-                            if i>=0 and abs(asteroids[i])>abs(asteroids[i+1]):
-                                asteroids.pop(i+1)
-                            else:
-                                asteroids.pop(i)
-                            i-=1
-                        this_collisions+=1
-                    else:
-                        
-                        pass
-                    
-                i+=1
-                #print("curr",asteroids)
-            if this_collisions==0:
-                break
-            else:
-                total_collisions+=this_collisions
-        return asteroids
+            collide=0
             
+            while i<len(asteroids)-1 and i>=0:
+                #print(i,asteroids)
+                if (asteroids[i]>0 and asteroids[i+1]>0) or (asteroids[i]<0 and asteroids[i+1]<0) or (asteroids[i]<0 and asteroids[i+1]>0):
+                    i+=1
+                else:
+                    if asteroids[i]>0 and asteroids[i+1]<0:
+                        if abs(asteroids[i])>abs(asteroids[i+1]):
+                            asteroids.pop(i+1)
+                        elif abs(asteroids[i])<abs(asteroids[i+1]):
+                            asteroids.pop(i)
+                        else:
+                            asteroids.pop(i)
+                            asteroids.pop(i)
+                            i-=1
+                        collide+=1
+                    else:
+                        i+=1
+            #print(i,asteroids)
+            if collide==0:
+                break
+        return asteroids
+
 
         
