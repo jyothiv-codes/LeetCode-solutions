b5c6e375ec5393a174b7bdda6484d5f2090e28a2
Jyothi V <57355660+jyothiv-codes@users.noreply.github.com>
2025-10-02 21:08:14 -0700

    Time: 11 ms (5.49%) | Memory: 17.8 MB (44.66%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,70 +1,63 @@
-from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        rotten=[]
-        rows=len(grid)
-        cols=len(grid[0])
-        fresh_oranges=0
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j]==2:
-                    pair=[i,j]
-                    rotten.append(pair)
-                if grid[i][j]==1:
-                    fresh_oranges+=1
-        
         time=0
-        nrot=0
-        while len(rotten)>0:
-            nrot=0
-            current_rotten = rotten[:]
-            rotten = []
-            for pair in current_rotten:
-                i,j=pair[0],pair[1]
-                #look right 
-                if j+1<cols:
-                    if grid[i][j+1]==1:
-                        new_rot=[i,j+1]
-                        grid[i][j+1]=2
-                        rotten.append(new_rot)
-                        nrot+=1
-                        fresh_oranges-=1
-                # left 
-                if j-1>=0:
-                    if grid[i][j-1]==1:
-                        new_rot=[i,j-1]
-                        grid[i][j-1]=2
-                        rotten.append(new_rot)
-                        nrot+=1
-                        fresh_oranges-=1
-                # up 
-                if i-1>=0:
-                    if grid[i-1][j]==1:
-                        new_rot=[i-1,j]
-                        grid[i-1][j]=2
-                        rotten.append(new_rot)
-                        nrot+=1
-                        fresh_oranges-=1
+        ones=0
+        queue=[]
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]==1:
+                    ones+=1
+                elif grid[i][j]==2:
+                    queue.append([i,j])
+        if ones==0:
+            return 0
+        print("current queue",queue)
+        while len(queue)>0:
+            arrange=0
+            n=len(queue)
+            k=0
+            while k<n:
+                node=queue.pop(0)
+                print("popped node",node)
+                i,j=node[0],node[1]
+                #check left 
+                if j>0 and grid[i][j-1]==1:
+                    arrange=1
+                    grid[i][j-1]=2
+                    queue.append([i,j-1])
+                    #print("value appended 1")
+                    ones-=1
+                #check right
+                if j+1<len(grid[0]) and grid[i][j+1]==1:
+                    arrange=1
+                    grid[i][j+1]=2
+                    queue.append([i,j+1])
+                    #print("value appended 2")
+                    ones-=1
+                #check up 
+                if i>0 and grid[i-1][j]==1:
+                    arrange=1
+                    grid[i-1][j]=2
+                    queue.append([i-1,j])
+                    #print("value appended 3")
+                    ones-=1
+                #check down 
+                if i+1<len(grid) and grid[i+1][j]==1:
+                    arrange=1
+                    grid[i+1][j]=2
+                    queue.append([i+1,j])
+                    #print("value appended 4")
+                    ones-=1
+                k+=1
+            if arrange==1:
+                time+=1
+            print("queue and time currently",queue,time)
+        print("number of ones is",ones)
+        if ones==0:
+            return time
+        return -1
 
-                #down 
-                if i+1<rows:
-                    if grid[i+1][j]==1:
-                        new_rot=[i+1,j]
-                        grid[i+1][j]=2
-                        rotten.append(new_rot)
-                        nrot+=1
-                        fresh_oranges-=1
-            
-                #grid[i][j]=-1
-                #rotten.pop(0)
-            if len(rotten) > 0:
-                time += 1
-                print(grid)
-                print("fresh oranges",fresh_oranges)
-        print("fresh oranges",fresh_oranges)
-        if fresh_oranges>0:
-            return -1
-        return time
+
 
             
 
