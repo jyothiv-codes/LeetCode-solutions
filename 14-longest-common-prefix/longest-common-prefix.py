a986d42dfea38681e9f451781c4035530799e353
Jyothi V <57355660+jyothiv-codes@users.noreply.github.com>
2025-10-02 21:39:38 -0700

    Time: 15 ms (4.67%) | Memory: 17.9 MB (30.40%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,24 +1,27 @@
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-        strs.sort()
-        compare=strs[0]
-        prefix=""
-        flag=False
-        i=len(compare)
-        while i>0:
-            for word in strs[1:]:
-                if compare[0:i] in word[0:len(compare[0:i])]:
-                    flag=True
-                else:
-                    flag=False
-                    break
-            if flag==True:
-                return compare[0:i]
-            i-=1
-        if len(strs)==1:
-            return strs[0]
-        return prefix
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        d={}
+        for word in strs:
+            i=len(word)-1
+            while i>=0:
+                d[word[:i+1]]=d.get(word[:i+1],0)+1
+                #if word[:i+1] in d:
+                    
+                i-=1
+        if len(d)==0:
+            return ""
+        m=max(d.values())
+        if m!=len(strs):
+            return ""
+        string=""
+        for key in d:
+            if d[key]==m:
+                if len(string)<len(key):
+                    string=key
+        print(d)
+        return string
+
+
+            
+
+        
